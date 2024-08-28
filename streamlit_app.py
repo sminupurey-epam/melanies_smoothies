db5318afebf035d76106e48ec540d582a36c9f3e
sminupurey-epam <santosh_minupurey@epam.com>
2024-08-28 17:26:12 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
 #st.write("Your favorite fruit is:", option)
 
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
